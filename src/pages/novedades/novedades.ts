c29299e3a5a98cbdbe24a72532df670f40f1351f
Felipe Arcos <efarcos@unicauca.edu.co>
2017-04-14 11:33:41 -0500

    implementacion de barra debajo de home en novedades

--- src/pages/novedades/novedades.ts
+++ src/pages/novedades/novedades.ts
@@ -3,9 +3,10 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import { Novedad, NovedadesData } from '../../providers/novedades-data';
 import { AddNovedadPage } from '../add-novedad/add-novedad';
+import { MapaPage } from '../mapa/mapa';
 
-import {DescripcionPage} from '../descripcion/descripcion';
-import {NovedadesService} from '../../providers/novedades-service';
+import { DescripcionPage } from '../descripcion/descripcion';
+import { NovedadesService } from '../../providers/novedades-service';
 
 
 @Component({
@@ -16,26 +17,34 @@ export class NovedadesPage {
 
   novedades: Novedad[]
 
-  constructor(public navCtrl: NavController, 
-  public navParams: NavParams, 
-  public service: NovedadesService) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public service: NovedadesService) {
     this.novedades = [];
   }
 
   ionViewDidEnter() {
     this.service.all().subscribe(data => this.novedades = data);
     console.log(this.novedades);
-    
+
   }
 
   goToAdd() {
     this.navCtrl.push(AddNovedadPage)
   }
 
-  goToDetail(id: string){
+  goToDetail(id: string) {
     this.navCtrl.push(DescripcionPage, {
       id: id
     })
   }
-  
+
+  goToNovedades() {
+    this.navCtrl.push(NovedadesPage)
+  }
+
+  goToMapa() {
+    this.navCtrl.push(MapaPage)
+  }
+
 }
--- src/providers/novedades-service.ts
+++ src/providers/novedades-service.ts
@@ -4,12 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { Novedad } from '../models/novedad';
 import { URL } from '../app/app.config';
 
-/*
-  Generated class for the NovedadesService provider.
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class NovedadesService {
 
