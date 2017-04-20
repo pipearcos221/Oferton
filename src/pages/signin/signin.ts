7e567d3125e689a253e17d042e3007204df027ed
José Luis Beltrán <jlbeltran94@gmail.com>
2017-04-19 21:28:03 -0500

    sigin implementado

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
+import { LoginService } from '../../providers/login-service';
+import { User } from '../../models/user';
+import { secret } from '../../app/app.config';
 
 
 @Component({
@@ -8,10 +11,37 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SigninPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  user: User;
+  pin: string;
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public toastCtrl: ToastController,
+    public service: LoginService) {
+    this.user = new User;    
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SigninPage');
+  }
+
+  sigin() {
+    if (this.pin == secret){
+      this.service.signin(this.user).subscribe(res =>{
+      if (res.success) {        
+        this.navCtrl.pop();    
+      } else {
+        if(res.exist){
+          this.toastCtrl.create({ message: "El nombre de usuario ya esta en uso", duration: 3000 }).present();
+        }        
+      }
+
+    }, err => {
+      console.log(JSON.stringify(err));
+    });    
+    }else{
+      this.toastCtrl.create({ message: "El pin es invalido", duration: 3000 }).present();
+    }
   }
 
 }
